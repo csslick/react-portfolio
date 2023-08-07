019c32eed1e85aa7defe6687773d69609ad122a5
csslick <digitone@naver.com>
2023-08-07 17:02:55 +0900

    Add files via upload

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export default function Header() {
+  const [toggle, setToggle] = useState(false)
+
+  return (
+    <header id='header'>
+      <h1>REACT PORTFOLIO</h1>
+      <nav className={toggle ? 'active': ''}>
+        <ul>
+          <li><a href="#intro">intro</a></li>
+          <li><a href="#about">about</a></li>
+          <li><a href="#skills">skills</a></li>
+          <li><a href="#works">works</a></li>
+          <li><a href="#contact">contact</a></li>
+        </ul>
+      </nav>
+      <button
+        onClick={ () => {setToggle(!toggle) }} 
+        className='mobile-toggle'>toggle</button>
+    </header>
+  )
+}
