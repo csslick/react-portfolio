7bf3dc2d2a3181394b9f6d47c28dedae2aa54751
csslick <digitone@naver.com>
2023-08-09 19:52:54 +0900

    Add files via upload

--- src/utils/lenis.ts
+++ src/utils/lenis.ts
@@ -4,9 +4,9 @@ const lenis = () => {
 
     const lenis = new Lenis();
 
-    lenis.on("scroll", (e:any) => {
-        // console.log(e);
-    });
+    // lenis.on("scroll", (e:any) => {
+    //     // console.log(e);
+    // });
 
     function raf(time:any) {
         lenis.raf(time);
